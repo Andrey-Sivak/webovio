2ecffd938e771d34286be4a1e2dc018428583b25
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2023-04-04 20:18:30 +0300

    refactor: extend tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -79,6 +79,8 @@ module.exports = {
 			maxWidth: {
 				container: '1280px',
 				620: '620px',
+				md: '475px',
+				'quote-card': '450px',
 			},
 			fontFamily: {
 				poppins: [
@@ -88,18 +90,38 @@ module.exports = {
 			},
 			fontSize: {
 				base: ['16px', '24px'],
+				'1.5xl': ['22px', '33px'],
+				'4xl': ['36px', '46px'],
 				'5xl': ['48px', '60px'],
 				'6xl': ['60px', '74px']
 			},
 			boxShadow: {
+				medium: '20px 25px 43px rgba(0, 0, 0, 0.18)',
 				huge: '50px 50px 100px rgba(0, 0, 0, 0.145947)'
 			},
 			colors: {
+				green: {
+					dirty: '#4F553D',
+					'dirty-light': '#5D6544',
+					light: '#B6C197',
+				},
+				black: {
+					200: '#213842',
+					DEFAULT: '#4D533C',
+				},
+				grey: {
+					200: '#FAFAFA',
+					300: '#D4D6D4',
+				},
 				yellow: '#FFD723',
 			},
 			zIndex: {
 				max: 1000000,
 			},
+			gridTemplateColumns: {
+				'90-1': '90px 1fr',
+				'46-1': '46px 1fr',
+			}
 		},
 	},
 	plugins: [],
