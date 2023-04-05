d4341c59b87a61a2826a665df02a0b34c92551e3
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2023-04-05 19:45:50 +0300

    refactor: animate first section

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,10 @@ import { Common } from './modules/Common.js';
 import * as flsFunctions from './modules/functions.js';
 import { Slider } from './Slider.js';
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger.js';
 import { Loader } from './modules/Loader.js';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 flsFunctions.isWebp();
 
@@ -12,3 +15,39 @@ window.webovioCommon = new Common();
 
 new Loader(gsap);
 new Slider('.slider');
+
+gsap.to('.hero-section-top-note', {
+	scrollTrigger: {
+		trigger: '.hero-section',
+		start: 'top top',
+		scrub: true,
+	},
+	yPercent: -500,
+});
+
+gsap.to('.hero-section-heading', {
+	scrollTrigger: {
+		trigger: '.hero-section',
+		start: 'top top',
+		scrub: true,
+	},
+	yPercent: -50,
+});
+
+gsap.to('.hero-section-text', {
+	scrollTrigger: {
+		trigger: '.hero-section',
+		start: 'top top',
+		scrub: true,
+	},
+	yPercent: -200,
+});
+
+gsap.to('.hero-section-link', {
+	scrollTrigger: {
+		trigger: '.hero-section',
+		start: 'top top',
+		scrub: true,
+	},
+	yPercent: -350,
+});
